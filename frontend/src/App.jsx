7fd553b1dbdb30eee082d62c22832093e4bf6102
TEJAAS2110 <23112190@PVGCOET.AC.IN>
2025-10-02 15:46:41 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react'
 import './App.css'
-import ChatInterface from './components/Chat/ChatInterface'
+import ChatInterface from './components/chat/ChatInterface.jsx';
 import { listDocuments, uploadDocument, checkHealth } from './services/api'
 
 function App() {
